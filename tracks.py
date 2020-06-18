2b1db48142793f490d6d0b5bf71ed852f8ce023e
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-06-18 23:08:37 +0000

    Codacy Changes

--- tracks.py
+++ tracks.py
@@ -1,5 +1,5 @@
-TRACKS= ["java", "python", "c", "cpp"]
+TRACKS = ["java", "python", "c", "cpp"]
 # Available (add as per your requirements):
-#   Languages: "ruby", "shell", "sql", "fp", 
+#   Languages: "ruby", "shell", "sql", "fp",
 #   Domians: "algorithms", "data-structures", "mathematics", "ai", "databases", "regex"
 #            "tutorials"
