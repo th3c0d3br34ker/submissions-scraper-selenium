eea148f2f6ad6318b51de7636418d921b4c095bf
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-06-24 20:06:00 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,19 @@
+class ACCOUNTS():
+    def __init__(self):
+        self.CodeChef = {
+            "username": "username",
+            "password": "password"
+        }
+
+        self.Hackerrank = {
+            "username": "username",
+            "password": "password",
+            "tracks":  ["ruby"]
+            # Available (add as per your requirements):
+            #   Languages: "java", "python", "c", "cpp", "ruby", "shell", "sql", "fp",
+            #   Domians: "algorithms", "data-structures", "mathematics", "ai", "databases", "regex"
+            #            "tutorials"
+        }
+
+    def getAccounts(self):
+        return vars(self)
